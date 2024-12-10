d86992e6b9de8c15258bbd0567f0543d8261202b
chris <christoph-heck@web.de>
2024-12-10 17:46:33 +0100

    memory leak fixed

--- src/main.cpp
+++ src/main.cpp
@@ -32,7 +32,7 @@ using namespace std;
 using torch::executor::Error;
 using torch::executor::Result;
 
-#define METHOD_ALLOCATOR_POOL_SIZE (1 * 1024)
+#define METHOD_ALLOCATOR_POOL_SIZE (512)
 uint8_t* method_allocator_pool;
 
 void et_pal_init(void) {}
@@ -67,94 +67,95 @@ void delay_ms(uint32_t ms) {
 
 
 int main() {
+	while(true){
 
-	torch::executor::runtime_init();
+		torch::executor::runtime_init();
 
-	ET_LOG(Info, "Model in %p %c", model_pte, model_pte[0]);
+		ET_LOG(Info, "Model in %p %c", model_pte, model_pte[0]);
 
-	method_allocator_pool = (uint8_t*)malloc(METHOD_ALLOCATOR_POOL_SIZE);
+		method_allocator_pool = (uint8_t*)malloc(METHOD_ALLOCATOR_POOL_SIZE);
 
-	auto loader =
-		torch::executor::util::BufferDataLoader(model_pte, sizeof(model_pte));
-	ET_LOG(Info, "Model PTE file loaded. Size: %lu bytes.", sizeof(model_pte));
-	Result<torch::executor::Program> program =
-		torch::executor::Program::load(&loader);
-	if (!program.ok()) {
-		ET_LOG(
-			Info,
-			"Program loading failed @ 0x%p: 0x%" PRIx32,
-			model_pte,
-			program.error());
-	}
+		auto loader =
+			torch::executor::util::BufferDataLoader(model_pte, sizeof(model_pte));
+		ET_LOG(Info, "Model PTE file loaded. Size: %lu bytes.", sizeof(model_pte));
+		Result<torch::executor::Program> program =
+			torch::executor::Program::load(&loader);
+		if (!program.ok()) {
+			ET_LOG(
+				Info,
+				"Program loading failed @ 0x%p: 0x%" PRIx32,
+				model_pte,
+				program.error());
+		}
 
-	ET_LOG(Info, "Model buffer loaded, has %lu methods", program->num_methods());
+		ET_LOG(Info, "Model buffer loaded, has %lu methods", program->num_methods());
 
-	const char* method_name = nullptr;
-	{
-		const auto method_name_result = program->get_method_name(0);
-		ET_CHECK_MSG(method_name_result.ok(), "Program has no methods");
-		method_name = *method_name_result;
-	}
-	ET_LOG(Info, "Running method %s", method_name);
+		const char* method_name = nullptr;
+		{
+			const auto method_name_result = program->get_method_name(0);
+			ET_CHECK_MSG(method_name_result.ok(), "Program has no methods");
+			method_name = *method_name_result;
+		}
+		ET_LOG(Info, "Running method %s", method_name);
 
-	Result<torch::executor::MethodMeta> method_meta =
-		program->method_meta(method_name);
-	if (!method_meta.ok()) {
-		ET_LOG(
-			Info,
-			"Failed to get method_meta for %s: 0x%x",
-			method_name,
-			(unsigned int)method_meta.error());
-	}
+		Result<torch::executor::MethodMeta> method_meta =
+			program->method_meta(method_name);
+		if (!method_meta.ok()) {
+			ET_LOG(
+				Info,
+				"Failed to get method_meta for %s: 0x%x",
+				method_name,
+				(unsigned int)method_meta.error());
+		}
 
-	torch::executor::MemoryAllocator method_allocator{
-		torch::executor::MemoryAllocator(
-			METHOD_ALLOCATOR_POOL_SIZE, method_allocator_pool)};
+		torch::executor::MemoryAllocator method_allocator{
+			torch::executor::MemoryAllocator(
+				METHOD_ALLOCATOR_POOL_SIZE, method_allocator_pool)};
 
-	std::vector<std::unique_ptr<uint8_t[]>> planned_buffers; // Owns the memory
-	std::vector<torch::executor::Span<uint8_t>>
-		planned_spans; // Passed to the allocator
-	size_t num_memory_planned_buffers = method_meta->num_memory_planned_buffers();
+		std::vector<std::unique_ptr<uint8_t[]>> planned_buffers; // Owns the memory
+		std::vector<torch::executor::Span<uint8_t>>
+			planned_spans; // Passed to the allocator
+		size_t num_memory_planned_buffers = method_meta->num_memory_planned_buffers();
 
-	for (size_t id = 0; id < num_memory_planned_buffers; ++id) {
-		size_t buffer_size =
-			static_cast<size_t>(method_meta->memory_planned_buffer_size(id).get());
-		ET_LOG(Info, "Setting up planned buffer %zu, size %zu.", id, buffer_size);
+		for (size_t id = 0; id < num_memory_planned_buffers; ++id) {
+			size_t buffer_size =
+				static_cast<size_t>(method_meta->memory_planned_buffer_size(id).get());
+			ET_LOG(Info, "Setting up planned buffer %zu, size %zu.", id, buffer_size);
 
-		planned_buffers.push_back(std::make_unique<uint8_t[]>(buffer_size));
-		planned_spans.push_back({planned_buffers.back().get(), buffer_size});
-	}
+			planned_buffers.push_back(std::make_unique<uint8_t[]>(buffer_size));
+			planned_spans.push_back({planned_buffers.back().get(), buffer_size});
+		}
 
-	torch::executor::HierarchicalAllocator planned_memory(
-		{planned_spans.data(), planned_spans.size()});
+		torch::executor::HierarchicalAllocator planned_memory(
+			{planned_spans.data(), planned_spans.size()});
 
-	torch::executor::MemoryManager memory_manager(
-		&method_allocator, &planned_memory);
+		torch::executor::MemoryManager memory_manager(
+			&method_allocator, &planned_memory);
 
-	Result<torch::executor::Method> method =
-		program->load_method(method_name, &memory_manager);
-	if (!method.ok()) {
-		ET_LOG(
-			Info,
-			"Loading of method %s failed with status 0x%" PRIx32,
-			method_name,
-			method.error());
-	}
-	ET_LOG(Info, "Method loaded.");
+		Result<torch::executor::Method> method =
+			program->load_method(method_name, &memory_manager);
+		if (!method.ok()) {
+			ET_LOG(
+				Info,
+				"Loading of method %s failed with status 0x%" PRIx32,
+				method_name,
+				method.error());
+		}
+		ET_LOG(Info, "Method loaded.");
 
-	ET_LOG(Info, "Preparing inputs...");
-	auto inputs = torch::executor::util::prepare_input_tensors(*method);
-	if (!inputs.ok()) {
-		ET_LOG(
-			Info,
-			"Preparing inputs tensors for method %s failed with status 0x%" PRIx32,
-			method_name,
-			inputs.error());
-	}
-	ET_LOG(Info, "Input prepared.");
-	while (true){
+		ET_LOG(Info, "Preparing inputs...");
+		auto inputs = torch::executor::util::prepare_input_tensors(*method);
+		if (!inputs.ok()) {
+			ET_LOG(
+				Info,
+				"Preparing inputs tensors for method %s failed with status 0x%" PRIx32,
+				method_name,
+				inputs.error());
+		}
+		ET_LOG(Info, "Input prepared.");
+
 		ET_LOG(Info, "Starting the model execution...");
-		delay_ms(100);
+		//delay_ms(100);
 		Error status = method->execute();
 		if (status != Error::Ok) {
 			ET_LOG(
@@ -189,10 +190,11 @@ int main() {
 			}
 		}
 		ET_LOG(Info, "Program complete, exiting.");
-		delay_ms(100); // Delays for 100 milliseconds
+		// Freeing the memory
+		free(method_allocator_pool);
+		method_allocator_pool = nullptr; // Optional but recommended to avoid dangling pointers
+		//delay_ms(100); // Delays for 100 milliseconds
 	}
-	
-	
 
 	return 0;
 }
