ff73d8be82f306c5b617a2aadbc01f3176279236
chris <christoph-heck@web.de>
2025-03-28 08:04:25 +0100

    update

--- src/model_executor/ModelExecutor.cpp
+++ src/model_executor/ModelExecutor.cpp
@@ -25,7 +25,7 @@
  * which are included in the bare metal build are also orchestrated by the
  * CMakeLists file. For example use see examples/arm/run.sh
  */
-#include "fcn/model_pte.h"
+#include "fcn_temp_ozone/model_pte.h"
 #include "utils/mbed_stats_wrapper.h"
 #include "model_executor/ModelExecutor.h"
 
